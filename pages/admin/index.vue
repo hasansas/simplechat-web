f81f9a8b6882e9e8b826d4c19e8f06eec7cb2609
hasansas <netter23@gmail.com>
2025-02-16 01:06:44 +0700

    add live chat

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -38,7 +38,11 @@
       <v-tabs-items v-model="chat.tabs">
         <v-tab-item>
           <v-list>
-            <v-list-item v-for="(item, index) in chat.rows" :key="index">
+            <v-list-item
+              v-for="(item, index) in chat.rows"
+              :key="index"
+              @click="selectChat(item)"
+            >
               <v-list-item-avatar
                 size="48"
                 :color="stringToColor(item.user.id)"
@@ -86,10 +90,10 @@
       class="grow d-flex align-center justify-center"
     >
       <div class="text-center">
-        <v-avatar size="80" color="grey lighten-3">
-          <v-icon size="32" color="grey">ri-message-3-line</v-icon>
-        </v-avatar>
-        <div class="mt-8">Please select a conversation</div>
+        <div class="logo-x-large logo-disable">
+          <Logo />
+        </div>
+        <div class="my-8">Please select a chat conversation</div>
       </div>
     </v-card>
     <v-navigation-drawer
@@ -106,8 +110,10 @@
               <v-icon size="16" dark> ri-user-3-line </v-icon>
             </v-list-item-avatar>
             <v-list-item-content>
-              <v-list-item-title> John Pixel </v-list-item-title>
-              <v-list-item-subtitle> Try chatbot conversation </v-list-item-subtitle>
+              <v-list-item-title>
+                {{ chat.selected.user.name }}
+              </v-list-item-title>
+              <v-list-item-subtitle> Not in contact </v-list-item-subtitle>
             </v-list-item-content>
           </v-list-item>
           <v-spacer></v-spacer>
@@ -119,7 +125,7 @@
       </template>
       <template v-slot:append>
         <v-card flat color="transparent" class="pa-4">
-          <v-card flat outlined  class="d-flex align-center pa-2 rounded-xxl">
+          <v-card flat outlined class="d-flex align-center pa-2 rounded-xxl">
             <v-btn icon>
               <v-icon>ri-attachment-2</v-icon>
             </v-btn>
@@ -134,7 +140,7 @@
         </v-card>
       </template>
       <v-list color="transparent" class="messages-container">
-        <template v-for="(item, index) in conversation">
+        <template v-for="(item, index) in chat.selected.conversation">
           <v-list-item :key="index">
             <div class="message" :class="item.isMe ? 'sent ml-auto' : 'received'">
               <div class="text">
@@ -148,23 +154,144 @@
     </v-navigation-drawer>
 
     <!-- contact info -->
-    <v-navigation-drawer v-model="contactInfo.drawer" right app width="400">
+    <v-navigation-drawer
+      v-if="chat.selected !== null"
+      v-model="contactInfo.drawer"
+      right
+      stateless
+      hide-overlay
+      app
+      width="400"
+    >
       <template v-slot:prepend>
-        <v-toolbar outlined flat color="white" style="margin-left:1px">
+        <v-toolbar outlined flat color="white" style="margin-left: 1px">
           <v-btn icon @click="contactInfo.drawer = false">
             <v-icon>ri-close-line</v-icon>
           </v-btn>
           <v-toolbar-title> Contact Info</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-tooltip left color="black">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                v-bind="attrs"
+                v-on="on"
+                depressed
+                fab
+                small
+                type="submit"
+                color="secondary"
+                class="mr-2"
+              >
+                <v-icon size="16">ri-user-add-line</v-icon>
+              </v-btn>
+            </template>
+            <span>Add to contact</span>
+          </v-tooltip>
         </v-toolbar>
       </template>
       <v-card flat class="text-center mt-16">
         <v-list-item-avatar size="160" color="blue lighten-3" class="mr-4">
           <v-icon size="32" dark> ri-user-3-line </v-icon>
         </v-list-item-avatar>
-        <v-card-title class="headline d-block">John Doe</v-card-title>
-        <div class="mt-8">
+        <v-card-title class="headline d-block">
+          {{ chat.selected.user.name }}
+        </v-card-title>
+        <v-card-subtitle> Not in contact </v-card-subtitle>
+        <v-card-text class="px-0">
+          <v-expansion-panels accordion multiple flat v-model="contactInfo.inputGroup">
+            <!-- Basic Info -->
+            <v-expansion-panel>
+              <v-expansion-panel-header class="subtitle-1">
+                Basic Info
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <!-- name -->
+                <v-text-field label="Name" outlined hide-details dense class="mb-1">
+                  <template v-slot:prepend-inner>
+                    <v-icon size="16" class="my-1">ri-user-line</v-icon>
+                  </template>
+                </v-text-field>
+
+                <!-- email -->
+                <v-text-field label="Email" outlined hide-details dense class="mb-1">
+                  <template v-slot:prepend-inner>
+                    <v-icon size="16" class="my-1">ri-at-line</v-icon>
+                  </template>
+                </v-text-field>
+
+                <!-- phone -->
+                <v-text-field label="Phone" outlined hide-details dense class="mb-1">
+                  <template v-slot:prepend-inner>
+                    <v-icon size="16" class="my-1">ri-phone-line</v-icon>
+                  </template>
+                </v-text-field>
+
+                <!-- Job title -->
+                <v-text-field label="Job title" outlined hide-details dense class="mb-1">
+                  <template v-slot:prepend-inner>
+                    <v-icon size="16" class="my-1">ri-pass-pending-line</v-icon>
+                  </template>
+                </v-text-field>
+              </v-expansion-panel-content>
+              <v-divider class="mx-6"></v-divider>
+            </v-expansion-panel>
+
+            <!-- Organization -->
+            <v-expansion-panel>
+              <v-expansion-panel-header class="subtitle-1">
+                Organization
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <!-- name -->
+                <v-text-field label="Name" outlined hide-details dense class="mb-1">
+                  <template v-slot:prepend-inner>
+                    <v-icon size="16" class="my-1">ri-building-line</v-icon>
+                  </template>
+                </v-text-field>
+
+                <!-- email -->
+                <v-text-field label="Email" outlined hide-details dense class="mb-1">
+                  <template v-slot:prepend-inner>
+                    <v-icon size="16" class="my-1">ri-at-line</v-icon>
+                  </template>
+                </v-text-field>
+
+                <!-- phone -->
+                <v-text-field label="Phone" outlined hide-details dense class="mb-1">
+                  <template v-slot:prepend-inner>
+                    <v-icon size="16" class="my-1">ri-phone-line</v-icon>
+                  </template>
+                </v-text-field>
+
+                <!-- Address -->
+                <v-text-field label="Address" outlined hide-details dense class="mb-1">
+                  <template v-slot:prepend-inner>
+                    <v-icon size="16" class="my-1">ri-map-pin-line</v-icon>
+                  </template>
+                </v-text-field>
+              </v-expansion-panel-content>
+              <v-divider class="mx-6"></v-divider>
+            </v-expansion-panel>
+
+            <!-- Custom Field -->
+            <v-expansion-panel>
+              <v-expansion-panel-header class="subtitle-1">
+                Custom Field
+              </v-expansion-panel-header>
+              <v-expansion-panel-content class="py-8">
+                <v-btn depressed outlined small color="grey darken-3">
+                  <span class="caption mr-4"> Add new</span>
+                  <v-icon size="16">ri-add-line</v-icon>
+                </v-btn>
+              </v-expansion-panel-content>
+              <v-divider class="mx-6"></v-divider>
+            </v-expansion-panel>
+          </v-expansion-panels>
+        </v-card-text>
+
+        <!-- <div class="mt-8">
           <v-btn depressed color="primary">Add to contact</v-btn>
-        </div>
+        </div> -->
       </v-card>
     </v-navigation-drawer>
   </v-sheet>
@@ -191,6 +318,7 @@ export default {
             lastMessage:
               "Morbi hendrerit augue eget tortor sagittis, sed fringilla ligula molestie",
             updatedAt: "2025-02-15T03:37:57.426Z",
+            conversation: [],
           },
           {
             id: "0153b78d-f5c1-42a2-9ffb-aa54a04193b4",
@@ -201,6 +329,7 @@ export default {
             status: "online",
             lastMessage: "Suspendisse aliquam semper",
             updatedAt: "2024-11-04T03:41:35.194Z",
+            conversation: [],
           },
           {
             id: "f7059c4e-b2a1-42eb-ac6f-3da9448e13f4",
@@ -212,26 +341,25 @@ export default {
             lastMessage:
               "Pellentesque imperdiet sem mauris, sit amet euismod sapien egestas eget",
             updatedAt: "2024-11-11T08:07:15.715Z",
+            conversation: [],
           },
         ],
         selected: {
-          id: "a2e2b218-136a-4241-aeea-2048bdfca5b7",
+          id: "f7059c4e-b2a1-42eb-ac6f-3da9448e13f4",
           user: {
-            id: "a2e2b218-136a-4241-aeea-2048bdfca5b7",
-            name: "John Pixel",
+            id: "f7059c4e-b2a1-42eb-ac6f-3da9448e13f4",
+            name: "Baba Yaga",
           },
-          status: "online",
+          status: "offline",
           lastMessage:
-            "Morbi hendrerit augue eget tortor sagittis, sed fringilla ligula molestie",
-          updatedAt: "2025-02-15T03:37:57.426Z",
+            "Pellentesque imperdiet sem mauris, sit amet euismod sapien egestas eget",
+          updatedAt: "2024-11-11T08:07:15.715Z",
+          conversation: [],
         },
       },
       conversation: [
         {
           id: "4f1d6e2b-d67e-4d65-81ad-4d464b1be581",
-          chatbotUsersId: "0fd92a35-882a-4c6f-98bc-f97c33f226f4",
-          stepId: "17eaf83a-043c-4caa-a37b-150f66a69b09",
-          stepType: "trigger",
           message: {
             body: "halo",
             type: "text",
@@ -241,9 +369,6 @@ export default {
         },
         {
           id: "74c1c57c-06dd-461c-bfb1-79f2ed1b87bb",
-          chatbotUsersId: "0fd92a35-882a-4c6f-98bc-f97c33f226f4",
-          stepId: "836cd69a-3f41-401d-94d9-f4df51d284ab",
-          stepType: "message",
           message: {
             sendTo: "6285743603758",
             type: "text",
@@ -255,9 +380,6 @@ export default {
         },
         {
           id: "75c065cb-2b05-4ff9-bab7-30ef94a7c0ae",
-          chatbotUsersId: "0fd92a35-882a-4c6f-98bc-f97c33f226f4",
-          stepId: "836cd69a-3f41-401d-94d9-f4df51d284ab",
-          stepType: "message",
           message: {
             sendTo: "6285743603758",
             type: "text",
@@ -270,18 +392,72 @@ export default {
       ],
       contactInfo: {
         drawer: true,
+        inputGroup: [0],
       },
     };
   },
   methods: {
+    selectChat(item) {
+      this.chat.selected = item;
+      this.contactInfo.drawer = true;
+    },
+    onChatMessageIncoming(data) {
+      const chats = this.chat.rows;
+      const chatId = data.chatId;
+      const message = data.message;
+      const conversation = {
+        id: "75c065cb-2b05-4ff9-bab7-30ef94a7c0ae",
+        message: message,
+        isMe: false,
+        createdAt: "2025-01-04T05:25:20.633Z",
+      };
+
+      // get chat
+      const chat = chats.find((e) => e.id === chatId);
+
+      if (typeof chat === "undefined") {
+        const newChat = {
+          id: chatId,
+          user: {
+            id: data.user.refId,
+            name: data.user.name,
+          },
+          status: "online",
+          lastMessage: message.body,
+          updatedAt: message.createdAt,
+          conversation: [conversation],
+        };
+
+        this.chat.rows.unshift(newChat);
+      } else {
+        chat.lastMessage = message.body;
+        chat.updatedAt = message.createdAt;
+        chat.conversation.push(conversation);
+      }
+    },
     stringToColor(str) {
       return Convert.stringToColor(str);
     },
+  },
+  created() {
+    this.$nuxt.$on("indexChatIncomingMessage", (data) => {
+      this.onChatMessageIncoming(data);
+    });
   },
 };
 </script>
 
 <style lang="scss">
+// form input
+.v-input {
+  .v-label {
+    &.v-label--active {
+      left: -28px !important;
+    }
+  }
+}
+
+// chat list
 .chats {
   .v-tab {
     text-transform: capitalize !important;
@@ -302,7 +478,7 @@ export default {
 // Chat screen
 .chat-screen {
   // background-color: #f3ede7 !important;
-  background-color: rgba(0,0,0,0.03) !important;
+  background-color: rgba(0, 0, 0, 0.03) !important;
 
   .messages-container {
     overflow-y: scroll;
